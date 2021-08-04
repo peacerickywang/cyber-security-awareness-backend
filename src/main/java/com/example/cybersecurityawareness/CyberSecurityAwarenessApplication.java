e1001417aed6fa78e6bb0f0c9f6ccb37eb314f14
peacericky <peacerickywang@gmail.com>
2021-08-04 10:58:22 +0800

    initial

---
+++ src/main/java/com/example/cybersecurityawareness/CyberSecurityAwarenessApplication.java
@@ -0,0 +1,13 @@
+package com.example.cybersecurityawareness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberSecurityAwarenessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberSecurityAwarenessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cybersecurityawareness/CyberSecurityAwarenessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cybersecurityawareness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberSecurityAwarenessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
